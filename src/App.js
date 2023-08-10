db61655355d4bc0322ce796047260ce5dbdb4ca6
26shubhapriya <shubhapriya1998@gmail.com>
2023-08-10 11:20:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import Cart from "./cart/Cart";
+import CartProvider from "./store/CartProvider"
+import Header from "./header/Header";
+import Medicine from "./medicine/Medicine";
+
 
 function App() {
+  const[showCart, setShowCart]= useState(false);
+
+  const showCartHandler=()=>{
+    setShowCart(true);
+  }
+  const closeCartHandler=()=>{
+    setShowCart(false);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CartProvider>
+    <Header onShowCart={showCartHandler}/>
+    {showCart && <Cart onClose={closeCartHandler}/>}
+    <main>
+    <Medicine/>
+    </main>
+    </CartProvider>
   );
-}
+} 
 
 export default App;
